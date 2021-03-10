cd2478fe14f5e60c749a9eaa3c9b6bcf88aa346f
chrishoran8 <horan_chris@hotmail.co.uk>
2021-03-10 14:21:32 +0000

    answers to exam

---
+++ Exam1/functions.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+#include "functions.h"
+
+float calcVat(float cost) {
+	return cost = cost + (cost / 100 * 20);
+}
--- Exam1/functions.h
+++ Exam1/functions.h
@@ -1,43 +1,5 @@
 
-
-
-//there are 30 marks awarded for this section
-
 #include <iostream>
 using namespace std;
 
-
-
-void sectionOneFunction()
-{
-    
-
-    //1) declare a type int called intOne (2)
-
-    //2) declare a type int called intTwo (2)
-
-    //3) declare a type int called intThree (2)
-
-    //4) declare type float called floatOne (2)
-
-    //5) assign intOne the value of 3 (2)
-
-    //6) assign intTwo the value of 5 (2)
-
-    //7) assign floatOne the value of 3.0f (2)
-
-    //8) use the multiplication operator multiply intOne with intTwo 
-    //use intThree to store the result (3)
-
-    //9) increment intThree  (3)
-
-    //10) initialise a type char called myChar to the value 'a' (2)
-
-    //11) output the value intThree to the screen  (3)
-    //ensure that when you cout, the next 
-
-    //12) declare a variable of type int called inputFromUser  (3)
-        
-    //13) obtain input from the user to assign inputFromUser. 
-    //Ensure you prompt the user to provide this input  (2)
-}
+float calcVat(float cost);
